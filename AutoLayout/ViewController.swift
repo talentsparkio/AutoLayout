25398108372f4eda7a854f8cd32b682f7ee9f156
Nick Chen <nchen.dev@mac.com>
2015-09-11 17:40:32 -0700

    Add example red box demo

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -15,26 +15,41 @@ class ViewController: UIViewController {
         
         let contentView = UIView()
         contentView.backgroundColor = UIColor.grayColor()
-        self.view = contentView
+        view = contentView
         
         let centerView = UIView()
         centerView.translatesAutoresizingMaskIntoConstraints = false
         centerView.backgroundColor = UIColor.blackColor()
-        self.view.addSubview(centerView)
+        view.addSubview(centerView)
         
         // Half of parent width
-        let halfWidth = centerView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.5)
+        let halfWidth = centerView.widthAnchor.constraintEqualToAnchor(view.widthAnchor, multiplier: 0.5)
         
         // Half of parent height
-        let halfHeight = centerView.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+        let halfHeight = centerView.heightAnchor.constraintEqualToAnchor(view.heightAnchor, multiplier: 0.5)
         
         // Center horizontally
-        let centerX = centerView.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor)
+        let centerX = centerView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor)
         
         // Center vertically
-        let centerY = centerView.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor)
-        
-        NSLayoutConstraint.activateConstraints([halfWidth, halfHeight, centerX, centerY])
+        let centerY = centerView.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor)
+
+        let redView = UIView()
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.backgroundColor = UIColor.redColor()
+        centerView.addSubview(redView)
+
+        // 10% of parent width
+        let redWidth = redView.widthAnchor.constraintEqualToAnchor(centerView.widthAnchor, multiplier: 0.1)
+
+        // 10% of parent height
+        let redHeight = redView.heightAnchor.constraintEqualToAnchor(centerView.heightAnchor, multiplier: 0.1)
+
+        let redX = centerView.leftAnchor.constraintEqualToAnchor(redView.leftAnchor)
+
+        let redY = centerView.topAnchor.constraintEqualToAnchor(redView.topAnchor)
+
+        NSLayoutConstraint.activateConstraints([halfWidth, halfHeight, centerX, centerY, redWidth, redHeight, redX, redY])
     }
     
     override func didReceiveMemoryWarning() {
